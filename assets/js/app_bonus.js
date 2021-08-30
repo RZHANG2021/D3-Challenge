ab6fb9a4659248d72c9182804f3296a9ed3fe81d
Rachel Zhang <mailrenxu@hotmail.com>
2021-08-30 23:01:54 +1000

    updates on xaxis

--- assets/js/app_bonus.js
+++ assets/js/app_bonus.js
@@ -17,8 +17,8 @@ var svg = d3
   .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
-  .attr("height", svgHeight);
-
+  .attr("height", svgHeight)
+  .attr("class", "chart");
 // Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
@@ -61,32 +61,61 @@ function renderCircles(Circles, newXScale, chosenXAxis) {
   return Circles;
 }
 
+
+//function used for updating state labels with a transition to new 
+function renderText(texts, newXScale, chosenXAxis) {
+
+  texts.transition()
+      .duration(1000)
+      .attr("x", d => newXScale(d[chosenXAxis]));
+      
+
+  return texts;
+}
+
+//function to swich x-axis values for tooltips
+function switchX(value, chosenXAxis) {
+
+  //stylize based on variable chosen
+  //poverty percentage
+  if (chosenXAxis === 'poverty') {
+      return `${value}%`;
+  }
+  //household income in dollars
+  else if (chosenXAxis === 'age') {
+      return `$${value}`;
+  }
+  //age (number)
+  else {
+      return `${value}`;
+  }
+}
 // function used for updating circles group with new tooltip
 function updateToolTip(chosenXAxis, Circles) {
 
-  var label;
+  var xlabel;
 
   if (chosenXAxis === "poverty") {
-    label = "In Poverty %:";
+    xlabel = "In Poverty %:";
   }
   else if (chosenXAxis === "age"){
-    label = "Age (median):";
+    xlabel = "Age (median):";
   }
   else {
-    label = "household Income (median):";
+    xlabel = "household Income (median):";
   }
   var toolTip = d3.tip()
     .attr("class", "d3-tip")
-    .offset([80, -60])
+    .offset([-10, 0])
     .html(function(d) {
-      return (`${d.state}<br>${label} ${d[chosenXAxis]}`);
+      return (`${d.state}<br>${xlabel} ${switchX(d[chosenXAxis])}`);
     });
 
     Circles.call(toolTip);
 
     Circles.on("mouseover", function(data) {
     toolTip.show(data);
-  })
+    })
     // onmouseout event
     .on("mouseout", function(data) {
       toolTip.hide(data);
@@ -97,6 +126,8 @@ function updateToolTip(chosenXAxis, Circles) {
 
 // Retrieve data from the CSV file and execute everything below
 d3.csv("assets/data/data.csv").then(function(stateData, err) {
+  
+  console.log(stateData);
   if (err) throw err;
 
   // parse data
@@ -129,62 +160,61 @@ d3.csv("assets/data/data.csv").then(function(stateData, err) {
 
   // append y axis
   chartGroup.append("g")
+    .classed("y-axis", true)
     .call(leftAxis);
 
-  // append initial circles
-//   var circlesGroup = chartGroup.selectAll("circle")
-//     .data(hairData)
-//     .enter()
-//     .append("circle")
-//     .attr("cx", d => xLinearScale(d[chosenXAxis]))
-//     .attr("cy", d => yLinearScale(d.num_hits))
-//     .attr("r", 20)
-//     .attr("fill", "pink")
-//     .attr("opacity", ".5");
 
-    var Circles = svg.selectAll("g", "Circles-group")
-      .data(stateData)
-      .enter();
-    Circles
+  // append inital circles
+  var Circles = chartGroup.selectAll("circle")
+    .data(stateData)
+    .enter()
     .append("circle")
+    .classed("stateCircle", true)
     .attr("cx", d => xLinearScale(d[chosenXAxis]))
     .attr("cy", d => yLinearScale(d.healthcare))
     .attr("r", "12")
     .attr("fill", "lightblue")
     .attr("opacity", ".75");
 
-    
-    Circles
+  
+  var texts = chartGroup.selectAll(".stateText")
+    .data(stateData)
+    .enter()
     .append("text")
+    .classed("stateText", true)
     .text(d => d.abbr)
     .attr('font-size',8)
-    .attr("dx", d => xLinearScale(d[chosenXAxis])-3)
-    .attr("dy", d => yLinearScale(d.healthcare)+3)
-    .attr("fill", "black");
+    .attr("x", d => xLinearScale(d[chosenXAxis]))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("dy", 4);
+    
 
 
   // Create group for three x-axis labels
-  var labelsGroup = chartGroup.append("g")
+  var xlabelsGroup = chartGroup.append("g")
     .attr("transform", `translate(${width / 2}, ${height + 20})`);
 
-  var povertyLabel = labelsGroup.append("text")
+  var povertyLabel = xlabelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 15)
     .attr("value", "poverty") // value to grab for event listener
+    .classed("aText", true)
     .classed("active", true)
     .text("In Poverty % ");
 
-  var ageLabel = labelsGroup.append("text")
+  var ageLabel = xlabelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 38)
     .attr("value", "age") // value to grab for event listener
+    .classed("aText", true)
     .classed("inactive", true)
     .text("Age (median):");
 
-    var incomeLabel = labelsGroup.append("text")
+    var incomeLabel = xlabelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 55)
     .attr("value", "income") // value to grab for event listener
+    .classed("aText", true)
     .classed("inactive", true)
     .text("HouseholdIncome (median):");
 
@@ -194,18 +224,18 @@ d3.csv("assets/data/data.csv").then(function(stateData, err) {
     .attr("y", 0 - margin.left)
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
-    .classed("axis-text", true)
+    .classed("aText", true)
     .text("Lacks of Healthcare %");
 
   // updateToolTip function above csv import
   var Circles = updateToolTip(chosenXAxis, Circles);
 
   // x axis labels event listener
-  labelsGroup.selectAll("text")
+  xlabelsGroup.selectAll("text")
     .on("click", function() {
       // get value of selection
       var value = d3.select(this).attr("value");
-      if (value !== chosenXAxis) {
+      if (value != chosenXAxis) {
 
         // replaces chosenXAxis with value
         chosenXAxis = value;
@@ -221,6 +251,9 @@ d3.csv("assets/data/data.csv").then(function(stateData, err) {
 
         // updates circles with new x values
         Circles = renderCircles(Circles, xLinearScale, chosenXAxis);
+
+        //update text with new x values
+        texts = renderText(texts, xLinearScale, chosenXAxis);
 
         // updates tooltips with new info
         Circles = updateToolTip(chosenXAxis, Circles);
